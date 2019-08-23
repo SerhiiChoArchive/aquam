48e93cbe318c79df4b24c8d484157f4858e3d1a7
SerhiiCho <serhiicho@protonmail.com>
2019-08-23 13:19:36 +0300

    Uncommeted code in upload.php

--- inc/upload.php
+++ inc/upload.php
@@ -8,7 +8,7 @@ require_once 'main.php';
 $file = Helper::getFileFromRequest($_FILES);
 $file || Helper::redirect('../index.php?msg=error');
 
-// $file_data = new CsvHandler($file['tmp_name']);
-// dd($file_data->getAll());
+$file_data = new CsvHandler($file['tmp_name']);
+dd($file_data->getAll());
 
 Helper::redirect('../index.php?msg=success');
