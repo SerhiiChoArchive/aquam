27651f85c03b747f0c685a40b8f46eef3152b83c
SerhiiCho <serhiicho@protonmail.com>
2019-08-25 16:38:58 +0300

    Added password to upload file form

--- app/Helper.php
+++ app/Helper.php
@@ -16,10 +16,17 @@ final class Helper
         exit;
     }
 
+    public static function passwordIsCorrect(array $request): bool
+    {
+        $config = require_once '../config.php';
+        return isset($request['password']) && $config->password === $request['password'];
+    }
+
     public static function getValidationMessage(): ?string
     {
         if ($_GET['msg'] === 'success') return 'Файл загружен и данный успешно обновленны!';
         if ($_GET['msg'] === 'error') return 'Формат файла должен быть формата csv!';
+        if ($_GET['msg'] === 'error_pwd_wrong') return 'Неправильный пароль';
 
         return null;
     }
--- inc/upload.php
+++ inc/upload.php
@@ -8,6 +8,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $file = Helper::getFileFromRequest($_FILES);
 $file || Helper::redirect('../?msg=error');
 
+if (!Helper::passwordIsCorrect($_POST)) {
+    Helper::redirect('../?msg=error_pwd_wrong');
+}
+
 $file_data = new CsvHandler($file['tmp_name']);
 $file_data->SaveData();
 
