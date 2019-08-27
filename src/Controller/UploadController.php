fd7af6f0a159119364db57b93494d742c91d98fd
SerhiiCho <serhiicho@protonmail.com>
2019-08-27 15:33:26 +0300

    Refactored UploadController and Helper class

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 use App\Converter;
 use App\CsvHandler;
 use App\Helper;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,20 +14,22 @@ final class UploadController extends AbstractController
 {
     /**
      * @Route("/upload", methods={"POST"})
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function upload()
+    public function upload(Request $request): RedirectResponse
     {
-        $file = Helper::getFileFromRequest($_FILES);
+        $file = $request->files->get('file');
 
-        if (!$file) {
+        if (!$file->isValid() || $file->getClientMimeType() !== 'application/vnd.ms-excel') {
             return $this->redirect('/?msg=error');
         }
 
-        if (!Helper::passwordIsCorrect($_POST)) {
+        if ($request->get('password') !== getenv('UPLOAD_PASSWORD')) {
             return $this->redirect('/?msg=error_pwd_wrong');
         }
 
-        $converter = new Converter($file['tmp_name']);
+        $converter = new Converter($file->getPathName());
         $file_data = new CsvHandler($converter->getCsvFilePath());
         $file_data->saveData();
 
--- src/Helper.php
+++ src/Helper.php
@@ -4,17 +4,6 @@ namespace App;
 
 final class Helper
 {
-    public static function getFileFromRequest(array $files): ?array
-    {
-        $file = $files['file'] ?? null;
-        return $file && !empty($file['type']) && $file['type'] === 'application/vnd.ms-excel' ? $file : null;
-    }
-
-    public static function passwordIsCorrect(array $request): bool
-    {
-        return isset($request['password']) && getenv('UPLOAD_PASSWORD') === $request['password'];
-    }
-
     public static function getValidationMessage(): ?string
     {
         $request = $_GET['msg'] ?? null;
