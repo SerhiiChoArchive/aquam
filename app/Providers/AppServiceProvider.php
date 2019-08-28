fae5d2f9d472296d49ab45382d586071c33a6232
SerhiiCho <serhiicho@protonmail.com>
2019-08-28 22:49:21 +0300

    Added https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,20 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        if (app()->env === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
