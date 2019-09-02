1e764754751023d47bd616c8932876202827357e
SerhiiCho <serhiicho@protonmail.com>
2019-09-02 11:37:02 +0300

    Added lang file date.php

---
+++ resources/lang/ru/date.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    'ago' => 'назад',
+    'less_than_hour' => 'меньше часа',
+    // Seconds
+    'second' => 'секунда',
+    'seconds' => 'секунды',
+    'seconds2' => 'секунд',
+    // Minutes
+    'minute' => 'минута',
+    'minutes' => 'минуты',
+    'minutes2' => 'минут',
+    // Hours
+    'hour' => 'час',
+    'hours' => 'часа',
+    'hours2' => 'часов',
+    // Days
+    'day' => 'день',
+    'days' => 'дня',
+    'days2' => 'дней',
+    // Weeks
+    'week' => 'неделя',
+    'weeks' => 'недели',
+    'weeks2' => 'недель',
+    // Months
+    'month' => 'месяц',
+    'months' => 'месяца',
+    'months2' => 'месяцев',
+    // Years
+    'year' => 'год',
+    'years' => 'года',
+    'years2' => 'лет',
+    // Decades
+    'decade' => 'десятилетие',
+    'decades' => 'десятилетия',
+    'decades2' => 'десятилетий',
+
+    // Months
+    'month_1' => 'Январь',
+    'month_2' => 'Февраль',
+    'month_3' => 'Март',
+    'month_4' => 'Апрель',
+    'month_5' => 'Май',
+    'month_6' => 'Июнь',
+    'month_7' => 'Июль',
+    'month_8' => 'Август',
+    'month_9' => 'Сентябрь',
+    'month_10' => 'Октябрь',
+    'month_11' => 'Ноябрь',
+    'month_12' => 'Декабрь',
+
+];
