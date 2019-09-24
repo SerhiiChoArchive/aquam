d3eea839e1ca86a6a6853466b4d3d996eb935dd0
SerhiiCho <serhiicho@protonmail.com>
2019-09-24 16:46:42 +0300

    Added materialize css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,42 +3,51 @@
 @section('title', 'Загрузить XLS файл')
 
 @section('content')
-    <div class="container">
-        <p><img src="{{ asset('images/logo-aqua.png') }}" alt="aqua-m" width="140"></p>
+    <div class="container" style="margin-top:34px">
+        <div class="center">
+            <img src="{{ asset('images/logo-aqua.png') }}" alt="aqua-m" width="140">
+        </div>
 
-        <form action="{{ action('UploadController@upload') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="input-field">
-                <label class="input-upload-label" for="input-file" data-title="✚ Выбрать XLS файл">
-                    <input type="file" name="file" id="input-file" required>
-                </label>
+        <div class="row">
+            <div class="col s12 l6">
+                <form action="{{ action('UploadController@upload') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="file-field input-field">
+                        <div class="btn btn-small teal darken-1">
+                            <span>Выбрать</span>
+                            <input type="file" name="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
 
-                <small class="input-upload-file-path">
-                    <b id="file-path"></b>
-                </small>
+                    <div class="input-field">
+                        <input type="password" name="password" id="password" placeholder="Пароль" required>
+                    </div>
+
+                    <div class="input-field">
+                        <button class="btn teal darken-1" style="display:block;width:100%" type="submit">Загрузить</button>
+                    </div>
+                </form>
             </div>
-            <div class="input-field">
-                <input type="password" name="password" id="password" placeholder="Пароль" required>
-            </div>
-            <button type="submit">Загрузить</button>
-        </form>
 
-        @if ($last_upload)
-            <div style="margin-top:50px">
-                <span>
-                    Последнее обновление:<br>
-                    <b style="color:#447e80">{{ $last_upload }}</b>
-                </span>
+            <div class="col s12 l6" style="padding-top:20px">
+                @if ($last_upload)
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Последнее обновление:</td>
+                                <td>{{ $last_upload }}</td>
+                            </tr>
+                            <tr>
+                                <td>Все визиты на прайс лист:</td>
+                                <td>{{ cache()->get('visits') }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                @endif
             </div>
-        @endif
-
-        {{-- Alert messages --}}
-        <div>
-            @if (session('error'))
-                <div class="message message-error">{{ session('error') }}</div>
-            @elseif (session('success'))
-                <div class="message message-success">{{ session('success') }}</div>
-            @endif
         </div>
     </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,12 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('main.css') }}">
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <title>@yield('title') | Aqua-m</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body>
     @yield('content')
+
+    {{-- Alert messages --}}
+    <script>
+        @if (session('error'))
+            M.toast({
+                html: "{{ session('error') }}",
+                classes: "red darken-2",
+            })
+        @elseif (session('success'))
+            M.toast({
+                html: "{{ session('success') }}",
+                classes: "green darken-2",
+            })
+        @endif
+    </script>
 </body>
-<script src="{{ asset('main.js') }}"></script>
 </html>
