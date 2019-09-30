4c0cc92a891b0ad787aa767e30978f9d60e3ce47
SerhiiCho <serhiicho@protonmail.com>
2019-09-30 21:07:08 +0300

    Fixed errror

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -114,7 +114,7 @@
                                                 <td>{{ $item->name }}</td>
                                                 <td>{{ $item->price }}</td>
                                                 <td>{{ $item->size }}</td>
-                                                <td><img src="{{ $item->image }}" width="120"></td>
+                                                <td><img src="{{ $item->image ?? '' }}" width="120"></td>
                                             </tr>
                                         @endforeach
                                     </tbody>
