4338b5f0fa634e71db1f986f8c84560570540905
SerhiiCho <serhiicho@protonmail.com>
2019-09-30 20:19:30 +0300

    Added form for updating images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,29 +4,21 @@
 
 @section('content')
     <div class="container" style="margin-top:34px">
-        <div class="center">
+        <div class="center" style="padding-bottom:30px">
             <img src="{{ asset('images/logo-aqua.png') }}" alt="aqua-m" width="170">
         </div>
 
         <div class="row">
             <div class="col s12 l6">
+                <p class="flow-text">Загрузить прайс лист</p>
+
                 <form action="{{ action('UploadController@upload') }}" method="POST" enctype="multipart/form-data">
                     @csrf
 
                     <div class="file-field input-field">
-                        <div class="btn  teal darken-1 waves-effect waves-light" style="min-width:220px">
-                            <span>Выбрать прайс</span>
+                        <div class="btn  teal darken-1 waves-effect waves-light">
+                            <span>Выбрать</span>
                             <input type="file" name="file">
-                        </div>
-                        <div class="file-path-wrapper">
-                            <input class="file-path validate" type="text">
-                        </div>
-                    </div>
-
-                    <div class="file-field input-field">
-                        <div class="btn teal darken-1 waves-effect waves-light" style="min-width:220px">
-                            <span>Выбрать изображения</span>
-                            <input type="file" name="images">
                         </div>
                         <div class="file-path-wrapper">
                             <input class="file-path validate" type="text">
@@ -38,7 +30,7 @@
                     </div>
 
                     <div class="input-field">
-                        <button class="btn teal darken-1 waves-effect waves-light" style="display:block;width:100%" type="submit">Загрузить</button>
+                        <button class="btn teal darken-1 waves-effect waves-light" type="submit">Загрузить</button>
                     </div>
                 </form>
 
@@ -52,6 +44,37 @@
                             <td>Последний визит в приложение:</td>
                             <td>{{ $last_request ?? '' }}</td>
                         </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col s12 l6">
+                <p class="flow-text">Прикрепить изображения</p>
+
+                <form action="{{ action('UploadController@uploadImages') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="file-field input-field">
+                        <div class="btn teal darken-1 waves-effect waves-light">
+                            <span>Выбрать</span>
+                            <input type="file" name="images">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+
+                    <div class="input-field">
+                        <input type="password" name="password" id="password" placeholder="Пароль" required>
+                    </div>
+
+                    <div class="input-field">
+                        <button class="btn teal darken-1 waves-effect waves-light" type="submit">Загрузить</button>
+                    </div>
+                </form>
+
+                <table style="margin-top:40px" class="striped responsive-table">
+                    <tbody>
                         <tr>
                             <td>Количество категорий:</td>
                             <td>{{ $price_categories_amount }}</td>
@@ -64,10 +87,10 @@
                 </table>
             </div>
 
-            <div class="col s12 l6" style="padding-top:50px">
+            <div class="col s12" style="padding-top:50px">
                 <ul class="collapsible">
                     @forelse ($price_list as $category => $items)
-                        <li>
+                        <li class="{{ $loop->first ? 'active' : null }}">
                             <div class="collapsible-header">
                                 <b class="teal-text darken-4" style="margin-right:5px">{{ count($items) }}</b>
                                 {{ $category }}
@@ -79,8 +102,9 @@
                                         <tr>
                                             <th>Номер</th>
                                             <th>Название</th>
-                                            <th>Размер</th>
                                             <th>Цена</th>
+                                            <th>Размер</th>
+                                            <th>Изображение</th>
                                         </tr>
                                     </thead>
                                     <tbody class="striped">
@@ -88,8 +112,9 @@
                                             <tr>
                                                 <td>{{ $item->number }}</td>
                                                 <td>{{ $item->name }}</td>
-                                                <td>{{ $item->size }}</td>
                                                 <td>{{ $item->price }}</td>
+                                                <td>{{ $item->size }}</td>
+                                                <td><img src="{{ $item->image }}" width="120"></td>
                                             </tr>
                                         @endforeach
                                     </tbody>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ Route::get('/', 'PageController@home');
 Route::get('/price-list', 'JsonController@priceList');
 Route::get('/info', 'JsonController@info');
 Route::post('/upload', 'UploadController@upload');
+Route::post('/upload-images', 'UploadController@uploadImages');
