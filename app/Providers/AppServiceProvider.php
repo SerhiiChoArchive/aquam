817c0d019072086d53557d052d8b00a6b2c6ccaf
SerhiiCho <serhiicho@protonmail.com>
2019-10-02 19:33:12 +0300

    Added to App service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -9,6 +10,8 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
