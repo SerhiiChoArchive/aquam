7eb5781151af9aebd30d05b540546168d0208c11
SerhiiCho <serhiicho@protonmail.com>
2019-10-02 17:26:47 +0300

    Changed update-data.blade.php file

--- resources/views/update-data.blade.php
+++ resources/views/update-data.blade.php
@@ -7,14 +7,15 @@
 <div class="container" style="margin-top:34px">
     <div class="row">
         <div class="col s12 l6">
-            <p class="flow-text">Прайс лист</p>
+            <h5>Обновить прайс лист</h5>
+            <p style="padding:10px 0">Выберите прайс лист формата .xls и нажмите "Обновить прайс лист"</p>
 
             <form action="{{ action('UploadController@upload') }}" method="POST" enctype="multipart/form-data">
                 @csrf
 
                 <div class="file-field input-field">
-                    <div class="btn teal darken-1 waves-effect waves-light">
-                        <span>Выбрать</span>
+                    <div class="btn transparent grey-text text-darken-4 waves-effect waves-light">
+                        <span>Выбрать файл</span>
                         <input type="file" name="file">
                     </div>
                     <div class="file-path-wrapper">
@@ -23,20 +24,21 @@
                 </div>
 
                 <div class="input-field">
-                    <button class="btn teal darken-1 waves-effect waves-light" type="submit">Загрузить</button>
+                    <button class="btn teal darken-1 waves-effect waves-light" type="submit">Обновить прайс лист</button>
                 </div>
             </form>
         </div>
 
         <div class="col s12 l6">
-            <p class="flow-text">Сохранить CSV</p>
+            <h5>Загрузить csv файл</h5>
+            <p style="padding:10px 0">Выберите файл формата .csv и нажмите "Загрузить csv файл" чтобы загрузить файл на сервер. При следующем обновлении прайс листа, ссылки к изображениям в этом файле будут применены к позициям по названию рыбы</p>
 
             <form action="{{ action('UploadController@uploadImages') }}" method="POST" enctype="multipart/form-data">
                 @csrf
 
                 <div class="file-field input-field">
-                    <div class="btn teal darken-1 waves-effect waves-light">
-                        <span>Выбрать</span>
+                    <div class="btn transparent grey-text text-darken-4 waves-effect waves-light">
+                        <span>Выбрать файл</span>
                         <input type="file" name="images">
                     </div>
                     <div class="file-path-wrapper">
@@ -45,7 +47,7 @@
                 </div>
 
                 <div class="input-field">
-                    <button class="btn teal darken-1 waves-effect waves-light" type="submit">Загрузить</button>
+                    <button class="btn teal darken-1 waves-effect waves-light" type="submit">Загрузить csv файл</button>
                 </div>
             </form>
         </div>
