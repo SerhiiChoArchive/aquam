8edefee85bfa8f9b7ed514f4be2bea1108397bb0
SerhiiCho <serhiicho@protonmail.com>
2019-12-28 18:56:56 +0200

    Changed mail.php config

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,7 +10,7 @@ class MailController extends Controller
 {
     public function create(Request $request)
     {
-        $res = Mail::to('serhiicho@protonmail.com')->send(new OrderMail);
+        $res = Mail::to('')->send(new OrderMail);
         var_dump($res);
         exit;
     }
