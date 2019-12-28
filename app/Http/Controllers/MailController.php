6515f38205d65549d6b1aa0fd2323a315d16c563
SerhiiCho <serhiicho@protonmail.com>
2019-12-28 19:02:26 +0200

    Testing stuff

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,8 +10,22 @@ class MailController extends Controller
 {
     public function create(Request $request)
     {
-        $res = Mail::to('')->send(new OrderMail);
-        var_dump($res);
-        exit;
+        function sendmail($_sSubject, $_sMessage, $_sEmail, $_sFrom, $_sReply, $_bPriority=false){
+            $subject = "=?utf-8?b?" . base64_encode($_sSubject) . "?=";
+            $headers  = "From: $_sFrom\r\n";
+            $headers .= "Reply-To: $_sReply\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            if($_bPriority){
+                $headers .= "X-Priority: 1 (Highest)\n";
+                $headers .= "X-MSMail-Priority: High\n";
+                $headers .= "Importance: High\n";
+            }
+            $headers .= "Content-type: text/html; charset=utf-8\r\n";
+            return mail($_sEmail, $subject, $_sMessage, $headers);
+        }
+        sendmail('Subject', 'some message', 'serhiicho@protonmail.com', 'office@aqua-m.com.ua', 'serhiicho@protonmail.com');
+//        $res = Mail::to('')->send(new OrderMail);
+//        var_dump($res);
+//        exit;
     }
 }
