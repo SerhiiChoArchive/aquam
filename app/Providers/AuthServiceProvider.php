b9cf7e95f00cd1f6b4808ed3ebb8414c4124e128
serhii <serhiicho@protonmail.com>
2020-10-25 14:46:12 +0200

    Moved declare(strict_types=1) to a separate line

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
