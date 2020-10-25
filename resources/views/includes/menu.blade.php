89d5f6d662f95a0e3c910b3abf99a17c79d9c95d
serhii <serhiicho@protonmail.com>
2020-10-25 14:48:29 +0200

    Replaced hard coded links with dynamic in menu.blade.php

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,5 +1,11 @@
 @auth
-    <li class="{{ active_if_route_is(['/']) }}"><a href="/">Панель</a></li>
-    <li class="{{ active_if_route_is(['update-data']) }}"><a href="/update-data">Обновить данные</a></li>
-    <li class=""><a href="javascript:" onclick="document.getElementById('logout-form').submit()">Выйти</a></li>
+    <li class="{{ active_if_route_is(['dashboard']) }}">
+        <a href="{{ action('DashboardController@index') }}">Панель</a>
+    </li>
+    <li class="{{ active_if_route_is(['update-data']) }}">
+        <a href="{{ action('UpdateDataController@index') }}">Обновить данные</a>
+    </li>
+    <li class="">
+        <a href="javascript:" onclick="document.getElementById('logout-form').submit()">Выйти</a>
+    </li>
 @endauth
