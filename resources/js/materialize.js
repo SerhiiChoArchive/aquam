74e6255dd85fe0f7687a94e6f21cdc935efe5c59
serhii <serhiicho@protonmail.com>
2020-10-27 16:22:39 +0200

    Added materialize lib

--- resources/js/materialize.js
+++ resources/js/materialize.js
@@ -1,5 +1,4 @@
-import 'materialize-css/dist/css/materialize.min.css'
-import 'materialize-css/dist/js/materialize.min.js';
+import 'materialize-css/dist/js/materialize.min.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     M.Collapsible.init(document.querySelectorAll('.collapsible'))
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
     <link rel="shortcut icon" href="{{ asset('storage/favicon.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>@yield('title') | Aqua-m</title>
-    <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     @include('includes.navbar')
     @yield('content')
 
     @include('includes.messages')
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
