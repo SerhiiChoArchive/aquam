5a13e7aaafb1e794d4d7fcd41d516db745e12a2f
serhii <serhiicho@protonmail.com>
2020-10-27 22:04:26 +0200

    Added needed api routs and actions to JsonController

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -4,20 +4,39 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
+use App\Models\PriceList;
 use Illuminate\Support\Facades\Cache;
 
 class JsonController extends Controller
 {
-    public function priceList(): Response
+    public function fish(): array
     {
-        return response(Cache::get('price-list'), 200, ['Content-Type' => 'application/json']);
+        return PriceList::query()->select('fish')->latest()->first()->fish;
     }
 
-    public function info()
+    public function equipment(): array
+    {
+        return PriceList::query()->select('equipment')->latest()->first()->equipment;
+    }
+
+    public function feed(): array
+    {
+        return PriceList::query()->select('feed')->latest()->first()->feed;
+    }
+
+    public function chemistry(): array
+    {
+        return PriceList::query()->select('chemistry')->latest()->first()->chemistry;
+    }
+
+    public function aquariums(): array
+    {
+        return PriceList::query()->select('aquariums')->latest()->first()->aquariums;
+    }
+
+    public function info(): string
     {
         Cache::put('last_request', date('Y-m-d H:i:s'));
-
-        return response(date('d.m.Y', strtotime(Cache::get('last_upload'))), 200);
+        return PriceList::query()->select('created_at')->latest()->first()->created_at->format('d.m.Y');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Models\PriceList;
 use App\Http\Controllers\JsonController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('price-list', [JsonController::class, 'priceList']);
+/**
+ * Keep it for legacy apps
+ * @deprecated
+ */
+Route::get('price-list', [JsonController::class, 'fish']);
+
+Route::get('fish', [JsonController::class, 'fish']);
+Route::get('equipment', [JsonController::class, 'equipment']);
+Route::get('feed', [JsonController::class, 'feed']);
+Route::get('chemistry', [JsonController::class, 'chemistry']);
+Route::get('aquariums', [JsonController::class, 'aquariums']);
+
 Route::get('info', [JsonController::class, 'info']);
