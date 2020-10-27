0de32863c6bdc3ab62081f4457e16cd9f0e826e7
serhii <serhiicho@protonmail.com>
2020-10-27 21:26:29 +0200

    Added equipment.blade.php

--- resources/views/includes/equipment.blade.php
+++ resources/views/includes/equipment.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col s12">
         <ul class="collapsible">
-            @forelse ($price_list->fish as $category => $items)
+            @forelse ($price_list->equipment as $category => $items)
                 <li>
                     <div class="collapsible-header">
                         <b class="teal-text darken-4" style="margin-right:5px">{{ count($items) }}</b>
@@ -12,20 +12,22 @@
                         <table class="striped responsive-table">
                             <thead>
                             <tr>
-                                <th>Номер</th>
+                                <th>Артикль</th>
                                 <th>Название</th>
+                                <th>Описание</th>
+                                <th>Производитель</th>
                                 <th>Цена</th>
-                                <th>Размер</th>
                                 <th>Изображение</th>
                             </tr>
                             </thead>
                             <tbody class="striped">
                                 @foreach ($items as $item)
                                     <tr>
-                                        <td>{{ $item['number'] }}</td>
+                                        <td>{{ $item['article'] }}</td>
                                         <td>{{ $item['name'] }}</td>
+                                        <td>{{ $item['description'] }}</td>
+                                        <td>{{ $item['producer'] }}</td>
                                         <td>{{ $item['price'] }}</td>
-                                        <td>{{ $item['size'] }}</td>
                                         <td>
                                             <div data-width="120"
                                                  class="async-load spinner"
