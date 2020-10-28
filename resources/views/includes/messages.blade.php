6fbf40bd78b56c70c2127faa04d462ee2ea5278d
serhii <serhiicho@protonmail.com>
2020-10-28 18:48:41 +0200

    Fixed messages.blade.php

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -1,22 +1,22 @@
 <script>
     @if (session('error'))
         M.toast({
-            html: "{{ session('error') }}",
+            html: `{{ session('error') }}`,
             classes: "red darken-2",
-            displayLength: 6000,
+            displayLength: 8000,
         })
     @elseif (session('success'))
         M.toast({
-            html: "{{ session('success') }}",
+            html: `{{ session('success') }}`,
             classes: "green darken-2",
-            displayLength: 6000,
+            displayLength: 5000,
         })
     @elseif (count($errors) > 0)
         @foreach ($errors->all() as $error)
             M.toast({
-                html: "{{ $error }}",
+                html: `{{ $error }}`,
                 classes: "red darken-2",
-                displayLength: 6000,
+                displayLength: 8000,
             })
         @endforeach
     @endif
