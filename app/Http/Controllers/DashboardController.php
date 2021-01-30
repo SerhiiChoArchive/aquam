e7812c77b337d33e1e18f03096b6ac6605c5af08
serhii <serhiicho@protonmail.com>
2021-01-30 17:06:05 +0200

    Updated packages

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-use Serhii\Ago\Time;
+use Serhii\Ago\TimeAgo;
 
 class DashboardController extends Controller
 {
@@ -17,8 +17,8 @@ class DashboardController extends Controller
     {
         return view('dashboard', [
             'price' => $price = auth()->user()->priceLists->last(),
-            'last_upload' => $price ? Time::ago($price->created_at->toDateTimeString()) : '-',
-            'last_request' => Time::ago(cache()->get('last_request')),
+            'last_upload' => $price ? TimeAgo::trans($price->created_at->toDateTimeString()) : '-',
+            'last_request' => TimeAgo::trans(cache()->get('last_request')),
         ]);
     }
 }
