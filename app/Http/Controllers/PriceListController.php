022dfd8bf3e570515829ef203c0b4ba752493b7a
serhii <serhiicho@protonmail.com>
2021-01-30 17:15:14 +0200

    Refactored PriceListController to support api version 2

--- app/Http/Controllers/PriceListController.php
+++ app/Http/Controllers/PriceListController.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\ConversionResult;
 use App\Helper;
 use App\Models\PriceList;
-use App\Converters\XlsxToArray;
+use App\Converters\V1\XlsxToArray as V1XlsxToArray;
+use App\Converters\V2\XlsxToArray as V2XlsxToArray;
 use Error;
 use Exception;
 use Illuminate\Contracts\View\View;
@@ -46,8 +48,6 @@ class PriceListController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
-        $request->validate(['file' => ['required']]);
-
         $new_file_name = sprintf("%s-%s.xlsx", date('Y-m-d_H-i-s'), time());
         $file = $request->file('file');
 
@@ -57,26 +57,35 @@ class PriceListController extends Controller
 
         $pathname = $file->move(storage_path('app/xlsx'), $new_file_name)->getPathname();
 
-        $converter = new XlsxToArray($pathname, new Xlsx());
+        $converter_v1 = new V1XlsxToArray($pathname, new Xlsx());
+        $converter_v2 = new V2XlsxToArray($pathname, new Xlsx());
 
         try {
-            $result = $converter->convert();
+            $result_v1 = $converter_v1->convert();
+            $result_v2 = $converter_v2->convert();
         } catch (Exception | SpreadsheetException | TypeError | Error $e) {
             Log::error($e->getMessage(), ['trace' => $e->getTraceAsString()]);
             return back()->with('error', $e->getMessage());
         }
 
+        $this->savePriceListWithVersion(1, $result_v1, $request->user()->id);
+        $this->savePriceListWithVersion(2, $result_v2, $request->user()->id);
+
+        Cache::forever('last_upload', date('Y-m-d H:i:s'));
+
+        return back()->with('success', 'Файл загружен и данный успешно обновленны!');
+    }
+
+    private function savePriceListWithVersion(int $api_version, ConversionResult $result, int $user_id): void
+    {
         PriceList::query()->create([
-            'user_id' => $request->user()->id,
+            'user_id' => $user_id,
             'fish' => $result->getFish(),
             'equipment' => $result->getEquipment(),
             'feed' => $result->getFeed(),
             'chemistry' => $result->getChemistry(),
             'aquariums' => $result->getAquariums(),
+            'api_version' => $api_version,
         ]);
-
-        Cache::forever('last_upload', date('Y-m-d H:i:s'));
-
-        return back()->with('success', 'Файл загружен и данный успешно обновленны!');
     }
 }
