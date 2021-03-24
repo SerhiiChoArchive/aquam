95c2752825b539243a18fe66f42ea61bbc671080
serhii <serhiicho@protonmail.com>
2021-03-24 18:29:35 +0200

    Working on uploading files

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ImagesController extends Controller
 {
     public function store(Request $request): int
     {
+        $file = $request->file('file');
+        $article = $request->get('article');
+        $file_name = Str::slug($article) . '.' . $file->getClientOriginalExtension();
+
+        $file->move(storage_path('app/public/uploads'), $file_name);
+
         return 1;
     }
 }
